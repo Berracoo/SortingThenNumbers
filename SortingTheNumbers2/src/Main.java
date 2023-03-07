49b70a09dc6abc2cf5b1d9a6a56da957533cd8e2
CoochiecooBerrauslu <berrauslu775@gmail.com>
2023-03-07 11:25:31 +0300

    -Added

---
+++ SortingTheNumbers2/src/Main.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //applying the scanner class
+        Scanner scanner = new Scanner(System.in);
+        int f,s,t;
+        System.out.print("Enter the first number : ");
+        f = scanner.nextInt();
+        System.out.println("********************");
+
+        System.out.print("Enter the second number : ");
+        s = scanner.nextInt();
+        System.out.println("********************");
+
+        System.out.print("Enter the third number : ");
+        t = scanner.nextInt();
+        System.out.println("********************");
+
+        // if the number that the user entered isn't an integer it will throw an exception
+        // try part works no matter what catch but works if our input  doesn't match with our integer type.
+
+        if(f < s && f < t){
+            if(s < t){
+                System.out.println("Sorting numbers as : f < s < t .");
+            }else{
+                System.out.println("Sorting numbers as : f < t < s .");
+            }
+
+        }else if(s < f && s < t){
+            if(f < t){
+                System.out.println("Sorting numbers as : s < f < t .");
+            }else{
+                System.out.println("Sorting numbers as : s < t < f .");
+            }
+        }else if(t < f && t < s){
+            if (f < s){
+                System.out.println("Sorting numbers as : t < f < s .");
+            }else{
+                System.out.println("Sorting numbers as : t < s < f .");
+            }
+        }else if( f == s && s == t){
+            System.out.println("Please be sure that you don't enter equal numbers !");
+        }
+
+    }
+}
